553825ea4931959f786ca8b8c2a7e8403e0baa03
Al <nguyenvtalexander@gmail.com>
2020-01-17 10:16:33 -0800

    memo change

--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -73,15 +73,18 @@ module.exports = {
 // Write a function to compute the number of combinations that make up that amount.
 // You may assume that you have infinite number of each kind of coin.
 
-var change = function (amount, coins) {
+var change = function (amount, coins, memo = {}) {
+    let key = amount + '-' + coins
+    if (memo[key]) return memo[key];
     if (amount === 0) return 1;
 
     let currentCoin = coins[coins.length - 1];
     let total = 0;
 
     for (let qty = 0; qty * currentCoin <= amount; ++qty) {
-        total += change(amount - qty * currentCoin, coins.slice(0, -1));
+        total += change(amount - qty * currentCoin, coins.slice(0, -1), memo);
     };
 
-    return total;
+    memo[key] = total;
+    return memo[key];
 }
