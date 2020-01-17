4e07a61cce43abcd0ee45659bef2a6ad64c9f6e4
Al <nguyenvtalexander@gmail.com>
2020-01-17 12:29:50 -0800

    select sort

--- p2.js
+++ p2.js
@@ -68,3 +68,35 @@ function minumumValueIndex(arr) {
 
     return minIndex;
 }
+
+// function selectionSort(arr) {
+//     for (let i = 0; i < arr.length; ++i) {
+//         let minIndex = i;
+
+//         for( let j = i + 1; i < arr.length; ++j) {
+//             if (arr[minIndex] > arr[j]) {
+//                 minIndex = j;
+//             }
+//         }
+
+//         swap(arr, i, minIndex);
+//     }
+
+//     return arr;
+// }
+// why wasn't this working?
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length; ++i) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < arr.length; ++j) {
+            if (arr[minIndex] > arr[j]) {
+                minIndex = j;
+            }
+        }
+
+        swap(arr, i, minIndex);
+    }
+    return arr;
+}
