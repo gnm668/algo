638dbc5a63eeee635e1d949aef9d85e798e6dce7
Al <nguyenvtalexander@gmail.com>
2020-01-16 19:24:49 -0800

    lucas memoi

--- memoization_project/lib/problems.js
+++ memoization_project/lib/problems.js
@@ -16,7 +16,12 @@
 // lucasNumberMemo(41)  // => 370248451
 // lucasNumberMemo(42)  // => 599074578
 function lucasNumberMemo(n, memo = {}) {
+    if (n === 0) return 2;
+    if (n === 1) return 1;
+    if (memo[n]) return memo[n];
 
+    
+    return memo[n] = lucasNumberMemo(n - 1, memo) + lucasNumberMemo(n - 2, memo);
 }
 
 
@@ -34,7 +39,7 @@ function lucasNumberMemo(n, memo = {}) {
 // minChange([1, 5, 10, 25], 15)    // => 2, because 10 + 5 = 15
 // minChange([1, 5, 10, 25], 100)   // => 4, because 25 + 25 + 25 + 25 = 100
 function minChange(coins, amount, memo = {}) {
-
+    
 }
 
 
