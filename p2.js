819a3045fadc00b236e1e92eac076e2ad8299303
Al <nguyenvtalexander@gmail.com>
2020-01-18 21:25:14 -0800

    palindrome 2 pointer

--- p2.js
+++ p2.js
@@ -185,3 +185,17 @@ function twoNumberSum(array, targetSum) {
     return [];
     // Write your code here.
 };
+
+function isPalindrome(string) {
+    let left = 0;
+    let right = string.length - 1;
+
+    while (left < right) {
+        if (string[left] !== string[right]) return false;
+        ++left;
+        --right;
+    };
+
+    return true;
+    // Write your code here.
+}
