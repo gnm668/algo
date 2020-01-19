e3fc05b0c7c30bf13c5ccd2ee0e2c8291349bcc1
Al <nguyenvtalexander@gmail.com>
2020-01-19 14:22:57 -0800

    alg

--- p3.py
+++ p3.py
@@ -45,10 +45,50 @@
 #time O(n)
 #space O(1)
 
-str = 'hello'
-length = len(str)
-print(length)
-print(str[0])
+# def bubbleSort(array):
+# 	isSorted = False
+	
+# 	while not isSorted:
+# 		isSorted = True
+		
+# 		for i in range(0, len(array) - 1):
+# 			if array[i] > array[i + 1]:
+# 				array[i], array[i +1] = array[i +1], array[i]
+# 				isSorted = False
+# 	return array
+
+#time O(n^2)
+#space O(1)
+
+# def insertionSort(array):	
+# 	for i in range(1, len(array)):
+# 		j = i 
+# 		while j > 0 and array[j] < array[j - 1]:
+# 			swap(j, j - 1, array)
+# 			j -= 1
+	
+# 	return array
+
+# def swap(i, j, arr):
+# 	arr[i], arr[j] = arr[j], arr[i]
+
+#time O(n^2) unless already sorted O(n)
+#space O(1)
+
+def caesarCipher(str, key):
+	alpha = list("abcdefghijklmnopqrstuvwxyz")
+	str = str.split()
+
+	print(str)
+
+caesarCipher('hello', 1)
+
+
+
+
+
+
+
 
 
 
