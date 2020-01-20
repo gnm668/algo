10d1d399cadb3cc79daca442b5dd372d298c133f
Al <nguyenvtalexander@gmail.com>
2020-01-19 22:08:37 -0800

    remove

--- p3.py
+++ p3.py
@@ -164,4 +164,9 @@ class DoublyLinkedList:
 			node = node.next
 		return node is not None   
 
-
+    def remove(self, node):
+		if node = self.head:
+			self.head = self.head.next
+		if node = self.tail:
+			self.tail = self.tail.prev
+		self.removeNodeBindings(node)
