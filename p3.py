90df0e701055ab34935690b140e4e6a870a3bce2
Al <nguyenvtalexander@gmail.com>
2020-01-21 23:01:42 -0800

    branch sums

--- p3.py
+++ p3.py
@@ -326,3 +326,20 @@ def findClosestValueInBst(tree, target, closest = float("inf")):
 	return closest
 
 # iterative is O(1) space 
+
+def branchSums(root):
+	sums = []
+	calculateBranchSums(root, 0, sums)
+	return sums
+
+def calculateBranchSums(node, runningSum, sums):
+	if node is None:
+		return
+	
+	newRunningSum = runningSum + node.value
+	if node.left is None and node.right is None:
+		sums.append(newRunningSum)
+		return
+		
+	calculateBranchSums(node.left, newRunningSum, sums)
+	calculateBranchSums(node.right, newRunningSum, sums)	
