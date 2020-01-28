07b02bd8361dd29bd1877bb01a9d185fc30cbb2e
Al <nguyenvtalexander@gmail.com>
2020-01-28 10:46:38 -0800

    warm up

--- p3.py
+++ p3.py
@@ -654,11 +654,29 @@ def hungry_rabbit(garden):
 
 # print(hungry_rabbit(garden))
 
-for x in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
-	print(x)
+# for x in [[-1, 0], [1, 0], [0, -1], [0, 1]]:
+# 	print(x)
 	
+def maxNonAdjSum(array):
+	if not len(array):
+		return 0
+	elif len(array) == 1:
+		return array[0]
+
+	first = array[0]
+	second = max(array[0], array[1])
+
+	for i in range(2, len(array)):
+		current = max(second, first + array[i])
+		first = second
+		second = current
+
+	return second
 
 
+x = [75, 105, 120, 75, 90, 135]
+
+print(maxNonAdjSum(x))
 
 
 
