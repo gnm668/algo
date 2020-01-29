feb7c35ad661e92b8ad008257d104b2e45e8f9bc
Al <nguyenvtalexander@gmail.com>
2020-01-29 12:10:06 -0800

    warmup

--- warmup.py
+++ warmup.py
@@ -280,11 +280,25 @@ def getLongestPalidrome(string, leftIdx, rightIdx):
 
 a = ['hi', 'look']
 def foo(words):
-	return max(words[0], words[1], key= lambda a: a[0] > a[1])
+	return max(words[0], words[1], key= lambda a: len(a))
 
 a = foo(a)
 print(a) 
 
+def maxSubsetSumNoAdjacent(array):
+	if not len(array):
+		return 0
+	elif len(array) == 1:
+		return array[0]
+	
+	second = array[0]
+	first = max(array[0], array[1])
+	
+	for i in range(2, len(array)):
+		current = max(first, second + array[i])
+		second = first
+		first = current
+	return first
 
 
 
