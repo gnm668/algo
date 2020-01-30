d5c36cb6fc8c9b82361d5830251ce9dd85cb5f0f
Al <nguyenvtalexander@gmail.com>
2020-01-29 17:45:56 -0800

    single cycle

--- medium.py
+++ medium.py
@@ -7,16 +7,16 @@
 
 def searchInSortedMatrix(matrix, target):
     row = 0
-	col = len(matrix[0]) - 1
-	
-	while row < len(matrix) and col >= 0:
-		if matrix[row][col] > target:
-			col -= 1
-		elif matrix[row][col] < target:
-			row += 1
-		else:
-			return [row, col]
-	return [-1, -1]
+    col = len(matrix[0]) - 1
+
+    while row < len(matrix) and col >= 0:
+        if matrix[row][col] > target:
+            col -= 1
+        elif matrix[    row][col] < target:
+            row += 1
+        else:
+            return [row, col]
+    return [-1, -1]
 
     # If sorted check possible coparisons
     # O(n + m) Time
@@ -64,29 +64,48 @@ def searchInSortedMatrix(matrix, target):
     #                 right -= 1
     #     return results
 
-    def smallestDifference(arrayOne, arrayTwo):
-	idxOne = 0
-	idxTwo = 0
-	arrayOne.sort()
-	arrayTwo.sort()
-	diff = float('inf')
-	curr = float('inf')
-	pair = []
-	
-	while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
-		firstNum = arrayOne[idxOne]
-		secondNum = arrayTwo[idxTwo]
-		
-		if firstNum < secondNum:
-			curr = secondNum - firstNum
-			idxOne += 1
-		elif firstNum > secondNum:
-			curr = firstNum - secondNum
-			idxTwo +=1
-		else:
-			return [firstNum, secondNum]
-		if diff > curr:
-			diff = curr
-			pair =[firstNum, secondNum]
-			
-	return pair
+def smallestDifference(arrayOne, arrayTwo):
+    idxOne = 0
+    idxTwo = 0
+    arrayOne.sort()
+    arrayTwo.sort()
+    diff = float('inf')
+    curr = float('inf')
+    pair = []
+
+    while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+        firstNum = arrayOne[idxOne]
+        secondNum = arrayTwo[idxTwo]
+
+        if firstNum < secondNum:
+            curr = secondNum - firstNum
+            idxOne += 1
+        elif firstNum > secondNum:
+            curr = firstNum - secondNum
+            idxTwo +=1
+        else:
+            return [firstNum, secondNum]
+        if diff > curr:
+            diff = curr
+        pair =[firstNum, secondNum]
+
+    return pair
+
+x = {1: None, 2: 2, 3: 3}
+print(x.values())
+
+
+def hasSingleCycle(array):
+    pos = 0
+    visit = {}
+    count = 0
+
+    while count < len(array) + 1:
+        visit[pos] = True
+        if count == 0:
+            visit.pop(0)
+        pos = (pos + array[pos]) % len(array)
+        count += 1
+    if len(visit.values()) == len(array):
+        return True
+    return False
