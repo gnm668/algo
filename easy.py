9f684ca26a6d536a52275e59b41b30b57b17c288
Al <nguyenvtalexander@gmail.com>
2020-01-31 17:17:55 -0800

    fib recursive

--- easy.py
+++ easy.py
@@ -23,3 +23,11 @@ def twoNumberSum(array, targetSum):
 
 print(twoNumberSum([1,2,3,9], 5))
 
+
+def getNthFib(n):
+    if n == 1:
+	    return 0
+    if n == 2:
+	    return 1
+	return getNthFib(n - 1) + getNthFib(n - 2)
+
