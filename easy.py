0958cbbd573a145f036ec10d2d4446a2f851886d
Al <nguyenvtalexander@gmail.com>
2020-01-31 17:10:38 -0800

    space solution

--- easy.py
+++ easy.py
@@ -1,12 +1,25 @@
 def twoNumberSum(array, targetSum):
-    differences = {}
-
-    for el in array:
-        difference = targetSum - el
-        if el in differences:
-            return [el, difference]
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum > targetSum:
+            right -= 1
+        elif currentSum < targetSum:
+            left += 1
         else:
-            differences[difference] = True
+            return [array[left], array[right]]
     return []
+    # differences = {}
+
+    # for el in array:
+    #     difference = targetSum - el
+    #     if el in differences:
+    #         return [el, difference]
+    #     else:
+    #         differences[difference] = True
+    # return []
 
 print(twoNumberSum([1,2,3,9], 5))
+
