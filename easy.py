fd3380eea8eab4f10ebf94e3801af6833c9154c0
Al <nguyenvtalexander@gmail.com>
2020-02-01 16:49:58 -0800

    insertion

--- easy.py
+++ easy.py
@@ -109,3 +109,16 @@ def bubbleSort(array):
 
 def swap(array, i, j):
 	array[i], array[j] = array[j], array[i]
+
+
+def insertionSort(array):
+	for i in range(1, len(array)):
+	j = i
+	while j > 0 and array[j] < array[j - 1]:
+	swap(array, j, j - 1)
+	j -= 1
+	return array
+
+
+def swap(array, i, j):
+	array[i], array[j] = array[j], array[i]
