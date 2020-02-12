6d280a91a95984886a0222547b5f918438204a96
Al <nguyenvtalexander@gmail.com>
2020-02-12 12:19:48 -0800

    syntax test

--- warmup.py
+++ warmup.py
@@ -311,11 +311,8 @@ a = {'a': 1, 'b': 2}
 b = [1,2,3,4]
 # print(b[4])
 
-board = list()
-for i in range(3):
-	board.append(list(' ') * 3)
-board[0][0] = 'x'
-print(board)
+board = {1: ' ', 2: ' ', 3: ' ', 4: ' ', 5: ' ', 6: ' ', 7: ' ', 8: ' ', 9: ' '}
+
 
 
 
