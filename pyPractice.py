0b6476079c26049474b25b985703fb5bb6698dc0
Al <nguyenvtalexander@gmail.com>
2020-02-23 23:02:46 -0800

    list methods

--- pyPractice.py
+++ pyPractice.py
@@ -5,8 +5,18 @@
 
 # print(num1 + num2)
 
-print(type(4))
-print(type([4]))
-print(type('4'))
-print(type(4.4))
-print(type({4: 4}))
+# print(type(4))
+# print(type([4]))
+# print(type('4'))
+# print(type(4.4))
+# print(type({4: 4}))
+
+#  list.index(ELEMENT)
+#  list.insert(INDEX, ELEMENT)
+#  del list[INDEX]
+# a = ['car', 'boat', 'airplane']
+# print(a.index('boat'))
+# a.insert(1, 'skate')
+# print(a)
+# del a[2]
+# print(a)
