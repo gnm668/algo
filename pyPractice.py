3f2596bbc0001fd30c898cb691b41b7961a1d8e4
Al <nguyenvtalexander@gmail.com>
2020-02-23 22:30:50 -0800

    t

--- pyPractice.py
+++ pyPractice.py
@@ -1,6 +1,12 @@
-print('pick a number')
-num1 = input()
-print('pick another number')
-num2 = input()
+# print('pick a number')
+# num1 = input()
+# print('pick another number')
+# num2 = input()
 
-print(num1 + num2)
+# print(num1 + num2)
+
+print(type(4))
+print(type([4]))
+print(type('4'))
+print(type(4.4))
+print(type({4: 4}))
