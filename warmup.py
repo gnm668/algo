5d6e94d9a4f54105d3742fcc49a78c6ce5e4d59e
Al <nguyenvtalexander@gmail.com>
2020-02-28 18:59:53 -0800

    tabulation mic change 2

--- warmup.py
+++ warmup.py
@@ -768,16 +768,29 @@ nums = [1,2,3,6]
 
 
 
-def minChange(coins, amount, memo = {}):
-	if amount == 0: return 0
-	numCoins = []
+# def minChange(coins, amount, memo = {}):
+# 	if amount == 0: return 0
+# 	numCoins = []
+
+# 	for coin in coins:
+# 		if coin <= amount:
+# 			numCoins.append(minChange(coins, amount - coin, memo) + 1)
+# 	memo[amount] = min(numCoins)
+# 	return memo[amount]
+
+def minChange(coins, amount):
+	table = [float('inf')] * (amount + 1)
+	table[0] = 0
 
 	for coin in coins:
-		if coin <= amount:
-			numCoins.append(minChange(coins, amount - coin, memo) + 1)
-	memo[amount] = min(numCoins)
-	return memo[amount]
-
+		for amt in range(len(table)):
+			qty = 0
+			while qty * coin <= amt:
+				remainder = amt - qty * coin 
+				attempt = table[remainder] + qty
+				if attempt < table[amt]: table[amt] = attempt
+				qty += 1
+	return table[-1]
 
 print(minChange([1, 2, 5], 10))
 
