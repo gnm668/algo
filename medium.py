7f7435dfce4084ae7bb75ee99b8bf605fba4ed60
Al <nguyenvtalexander@gmail.com>
2020-03-01 09:41:50 -0800

    three sum

--- medium.py
+++ medium.py
@@ -367,26 +367,47 @@ def minChange(coins, amount):
 #     return table[1]
 # print(lucasNum(40))
 
-def smallestDiff(li1, li2):
-    li1.sort()
-    li2.sort()
-    idx1, idx2 = 0, 0 
-    curr, diff = float('inf'), float('inf')
-    pair = []
+# def smallestDiff(li1, li2):
+#     li1.sort()
+#     li2.sort()
+#     idx1, idx2 = 0, 0 
+#     curr, diff = float('inf'), float('inf')
+#     pair = []
 
-    while idx1 < len(li1) and idx2 < len(li2):
-        num1, num2 = li1[idx1], li2[idx2]
-        if num1 > num2:
-            curr = num1 - num2
-            idx2 += 1
-        elif num1 < num2:
-            curr = num2 - num1
-            idx1 += 1
-        else:
-            return [num1, num2]
-        if curr < diff:
-            diff = curr
-            pair = [num1, num2]
-    return pair
+#     while idx1 < len(li1) and idx2 < len(li2):
+#         num1, num2 = li1[idx1], li2[idx2]
+#         if num1 > num2:
+#             curr = num1 - num2
+#             idx2 += 1
+#         elif num1 < num2:
+#             curr = num2 - num1
+#             idx1 += 1
+#         else:
+#             return [num1, num2]
+#         if curr < diff:
+#             diff = curr
+#             pair = [num1, num2]
+#     return pair
 
-print(smallestDiff([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
+# print(smallestDiff([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
+
+def threeSum(li, tar):
+    li.sort()
+    res = []
+    for i in range(len(li) - 2):
+        left = i + 1
+        right = len(li) - 1
+        while left < right:
+            currSum = li[i] + li[left] + li[right]
+            if currSum == tar:
+                res.append([li[i], li[left], li[right]])
+                left += 1
+                right -= 1
+            elif currSum > tar:
+                right -= 1
+            elif currSum < tar:
+                left += 1
+    return res
+
+print(threeSum([6,2,1,3,5,6,0], 8))
+
