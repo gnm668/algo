49b7b343d1155d2a82df3fb82093d66d6034ccc5
Al <nguyenvtalexander@gmail.com>
2020-03-01 21:45:09 -0800

    move el to end

--- medium.py
+++ medium.py
@@ -631,25 +631,38 @@ x = { 1:'a', 2:'b'}
 
 # print(threeSum([6,2,1,3,5,6,0], 8))
 
-def smallestDiff(li1, li2):
-    li1.sort()
-    li2.sort()
-    idx1, idx2 = 0, 0
-    diff, curr = float('inf'), float('inf')
-    pair = []
-    while idx1 < len(li1) and idx2 < len(li2):
-        num1, num2 = li1[idx1], li2[idx2]
-        if num1 > num2:
-            curr = num1 - num2
-            idx2 += 1
-        elif num1 < num2:
-            curr = num2 - num1
-            idx1 += 1
-        else:
-            return [num1, num2]
-        if curr < diff:
-            diff = curr
-            pair = [num1, num2]
-    return pair
+# def smallestDiff(li1, li2):
+#     li1.sort()
+#     li2.sort()
+#     idx1, idx2 = 0, 0
+#     diff, curr = float('inf'), float('inf')
+#     pair = []
+#     while idx1 < len(li1) and idx2 < len(li2):
+#         num1, num2 = li1[idx1], li2[idx2]
+#         if num1 > num2:
+#             curr = num1 - num2
+#             idx2 += 1
+#         elif num1 < num2:
+#             curr = num2 - num1
+#             idx1 += 1
+#         else:
+#             return [num1, num2]
+#         if curr < diff:
+#             diff = curr
+#             pair = [num1, num2]
+#     return pair
 
-print(smallestDiff([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
+# print(smallestDiff([-1, 5, 10, 20, 28, 3], [26, 134, 135, 15, 17]))
+
+def moveElementToEnd(li, toMove):
+    i = 0
+    j = len(li) - 1
+    while i < j:
+        while i < j and li[j] == toMove:
+            j -= 1
+        if li[i] == toMove:
+            li[i], li[j] = li[j], li[i]
+        i += 1
+    return li
+
+print(moveElementToEnd([2,1,2,2,2,4,3,2], 2))
