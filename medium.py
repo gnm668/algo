7d973d1b67c3b0926880cdc69f997b15b1fe49f4
Al <nguyenvtalexander@gmail.com>
2020-03-17 15:13:11 -0700

    min change tabulation

--- medium.py
+++ medium.py
@@ -1146,15 +1146,31 @@
 
 # print(permutations([1,2,3]))
 
-def minChange(coins, amount, memo = {0: 0}):
-    if amount in memo: return memo[amount]
-    numCoins = []
+# def minChange(coins, amount, memo = {0: 0}):
+#     if amount in memo: return memo[amount]
+#     numCoins = []
+
+#     for coin in coins:
+#         if coin <= amount:
+#             numCoins.append(minChange(coins, amount - coin, memo) + 1)
+    
+#     memo[amount] = min(numCoins)
+#     return memo[amount]
+
+def minChange(coins, amount):
+    table = [float("inf") for x in range(amount + 1)]
+    table[0] = 0
 
     for coin in coins:
-        if coin <= amount:
-            numCoins.append(minChange(coins, amount - coin, memo) + 1)
+        for amt in range(len(table)):
+            qty = 0
+            while qty * coin <= amt:
+                diff = amt - qty * coin
+                attempt = qty + table[diff]
+                if attempt < table[amt]: table[amt] = attempt
+                qty += 1
     
-    memo[amount] = min(numCoins)
-    return memo[amount]
+    return table[-1]
 
-print(minChange([1,2], 9))
+print(minChange([2,5,7], 25))
+
