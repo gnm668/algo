3ddfee86241335e1313610e78d48658fc0c9eba8
Al <nguyenvtalexander@gmail.com>
2020-04-17 01:03:50 -0700

    alana help

--- p2.js
+++ p2.js
@@ -1,79 +1,96 @@
-//tabulation notes
+// //tabulation notes
 
-function tabFib(n) {
-    let table = new Array(n);
+// function tabFib(n) {
+//     let table = new Array(n);
 
-    table[0] = 0;
-    table[1] = 1;
+//     table[0] = 0;
+//     table[1] = 1;
 
-    for (let i = 2; i <= n; ++i) {
-        table[i] = table[i - 1] + table[i -2];
-    }
+//     for (let i = 2; i <= n; ++i) {
+//         table[i] = table[i - 1] + table[i -2];
+//     }
 
-    return table[n];
-}
+//     return table[n];
+// }
 
-function tabFib2(n) {
-    let secondLast = 0;
-    let last = 1;
+// function tabFib2(n) {
+//     let secondLast = 0;
+//     let last = 1;
 
-    if (n === 0) return secondLast;
-    if (n === 1) return last;
+//     if (n === 0) return secondLast;
+//     if (n === 1) return last;
 
-    for (let i = 2; i <= n; ++i) {
-            let temp = last;
-            last = last + secondLast;
-            secondLast = temp;
-    }
+//     for (let i = 2; i <= n; ++i) {
+//             let temp = last;
+//             last = last + secondLast;
+//             secondLast = temp;
+//     }
 
-    return last;
-}
+//     return last;
+// }
 
-//bubble sort notes
+// //bubble sort notes
 
-function swap(arr, idx1, idx2) {
-    let temp = arr[idx1];
-    arr[idx1] = arr[idx2];
-    arr[idx2] = temp;
-}
+// function swap(arr, idx1, idx2) {
+//     let temp = arr[idx1];
+//     arr[idx1] = arr[idx2];
+//     arr[idx2] = temp;
+// }
 
-function bubbleSort(arr) {
-    let sorted = false;
+// function bubbleSort(arr) {
+//     let sorted = false;
 
-    while (!sorted) {
-        sorted = true;
+//     while (!sorted) {
+//         sorted = true;
 
-        for (let i = 0; i < arr.length; ++i) {
-            if (arr[i] > arr[i + 1]) {
-                swap(arr, i, i + 1);
-                sorted = false;
-            }
-        }
-    }
+//         for (let i = 0; i < arr.length; ++i) {
+//             if (arr[i] > arr[i + 1]) {
+//                 swap(arr, i, i + 1);
+//                 sorted = false;
+//             }
+//         }
+//     }
 
-    return arr;
+//     return arr;
 
-}
+// }
 
-//selection sort 
+// //selection sort 
 
-function minumumValueIndex(arr) {
-    let minIndex = 0;
+// function minumumValueIndex(arr) {
+//     let minIndex = 0;
 
-    for (let j = 0; j < arr.length; j++) {
-        if (arr[minIndex] > arr[j]) {
-            minIndex = j;
-        }
-    }
+//     for (let j = 0; j < arr.length; j++) {
+//         if (arr[minIndex] > arr[j]) {
+//             minIndex = j;
+//         }
+//     }
 
-    return minIndex;
-}
+//     return minIndex;
+// }
+
+// // function selectionSort(arr) {
+// //     for (let i = 0; i < arr.length; ++i) {
+// //         let minIndex = i;
+
+// //         for( let j = i + 1; i < arr.length; ++j) {
+// //             if (arr[minIndex] > arr[j]) {
+// //                 minIndex = j;
+// //             }
+// //         }
+
+// //         swap(arr, i, minIndex);
+// //     }
+
+// //     return arr;
+// // }
+// // why wasn't this working?
 
 // function selectionSort(arr) {
 //     for (let i = 0; i < arr.length; ++i) {
 //         let minIndex = i;
 
-//         for( let j = i + 1; i < arr.length; ++j) {
+//         for (let j = i + 1; j < arr.length; ++j) {
 //             if (arr[minIndex] > arr[j]) {
 //                 minIndex = j;
 //             }
@@ -81,122 +98,124 @@ function minumumValueIndex(arr) {
 
 //         swap(arr, i, minIndex);
 //     }
-
 //     return arr;
 // }
-// why wasn't this working?
 
-function selectionSort(arr) {
-    for (let i = 0; i < arr.length; ++i) {
-        let minIndex = i;
-
-        for (let j = i + 1; j < arr.length; ++j) {
-            if (arr[minIndex] > arr[j]) {
-                minIndex = j;
-            }
-        }
-
-        swap(arr, i, minIndex);
-    }
-    return arr;
-}
-
-function insertionSort(arr) {
-    for (let i = 1; i < arr.length; ++i) {
-        let currElement = arr[i];
-        for (var j = i - 1; j >= 0 && currElement < arr[j]; --j) {
-            arr[j + 1] = arr[j];
-        }
-        arr[j + 1] = currElement;
-    }
-    return arr;
+// function insertionSort(arr) {
+//     for (let i = 1; i < arr.length; ++i) {
+//         let currElement = arr[i];
+//         for (var j = i - 1; j >= 0 && currElement < arr[j]; --j) {
+//             arr[j + 1] = arr[j];
+//         }
+//         arr[j + 1] = currElement;
+//     }
+//     return arr;
     
+// }
+
+
+// //does the --j still decrement j at the end of inner loop before line 110?
+// //YES
+
+// function transpose(arr) {
+//     let rows = arr.length;
+//     let cols = arr[0].length;
+//     let grid = [];
+
+//     for (let i = 0; i < cols; ++i) {
+//         grid[i] = new Array(rows);
+//     };
+
+//     for (let i = 0; i < rows; ++i) {
+//         for (let j = 0; j < cols; ++j) {
+//             grid[j][i] = arr[i][j];
+//         };
+//     };
+
+//     return grid;
+// };
+
+// function revStr(str) {
+//     let i = 0, j = str.length - 1;
+
+//     for(let n = 0; n < str.length/2; ++n) {
+//         let temp = str[j];
+//         str[j] = str[i];
+//         str[i] = temp;
+//         ++ i;
+//         -- j;
+//     };
+
+//     return str;
+// };
+
+// let revStr = str => {
+
+//     for (let i = 0, j = str.length - 1; i < j; ++i, --j) {
+//         let temp = str[j];
+//         str[j] = str[i];
+//         str[i] = temp;
+//     };
+// };
+
+// function twoNumberSum(array, targetSum) {
+//     let diff = {};
+
+//     for (let i = 0; i < array.length; ++i) {
+//         if (targetSum - array[i] in diff) return [targetSum - array[i], diff[targetSum - array[i]]];
+//         diff[array[i]] = targetSum - array[i];
+//     };
+//     // Write your code here.
+//     return [];
+// };
+
+// function twoNumberSum(array, targetSum) {
+//     array = array.sort();
+//     let left = 0;
+//     let right = array.length - 1;
+
+//     while (left < right) {
+//         if (array[left] + array[right] === targetSum) {
+//             return [array[left], array[right]];
+//         } else if (array[left] + array[right] < targetSum) {
+//             ++left;
+//         } else if (array[left] + array[right] > targetSum) {
+//             --right
+//         };
+//     };
+//     return [];
+//     // Write your code here.
+// };
+
+// function isPalindrome(string) {
+//     let left = 0;
+//     let right = string.length - 1;
+
+//     while (left < right) {
+//         if (string[left] !== string[right]) return false;
+//         ++left;
+//         --right;
+//     };
+
+//     return true;
+//     // Write your code here.
+// } 
+
+
+var checkLock = function (first, second, third, fourth) {
+    if (!(first === 5 || first === 3 || first === 7)) {
+        return "incorrect"
+    } else if (second !== 2) {
+        return "incorrect"
+    } else if (third < 5 || third > 100) {
+        return "incorrect"
+    } else if (fourth >= 9 && fourth <= 20) {
+        return "incorrect"
+    }
+    return "correct"
 }
 
+console.log(checkLock(5, 2, 45, 1))
+console.log(checkLock(1, 2, 45, 1))
+console.log(checkLock(5, 2, 45, 10))
 
-//does the --j still decrement j at the end of inner loop before line 110?
-//YES
-
-function transpose(arr) {
-    let rows = arr.length;
-    let cols = arr[0].length;
-    let grid = [];
-
-    for (let i = 0; i < cols; ++i) {
-        grid[i] = new Array(rows);
-    };
-
-    for (let i = 0; i < rows; ++i) {
-        for (let j = 0; j < cols; ++j) {
-            grid[j][i] = arr[i][j];
-        };
-    };
-
-    return grid;
-};
-
-function revStr(str) {
-    let i = 0, j = str.length - 1;
-
-    for(let n = 0; n < str.length/2; ++n) {
-        let temp = str[j];
-        str[j] = str[i];
-        str[i] = temp;
-        ++ i;
-        -- j;
-    };
-
-    return str;
-};
-
-let revStr = str => {
-
-    for (let i = 0, j = str.length - 1; i < j; ++i, --j) {
-        let temp = str[j];
-        str[j] = str[i];
-        str[i] = temp;
-    };
-};
-
-function twoNumberSum(array, targetSum) {
-    let diff = {};
-
-    for (let i = 0; i < array.length; ++i) {
-        if (targetSum - array[i] in diff) return [targetSum - array[i], diff[targetSum - array[i]]];
-        diff[array[i]] = targetSum - array[i];
-    };
-    // Write your code here.
-    return [];
-};
-
-function twoNumberSum(array, targetSum) {
-    array = array.sort();
-    let left = 0;
-    let right = array.length - 1;
-
-    while (left < right) {
-        if (array[left] + array[right] === targetSum) {
-            return [array[left], array[right]];
-        } else if (array[left] + array[right] < targetSum) {
-            ++left;
-        } else if (array[left] + array[right] > targetSum) {
-            --right
-        };
-    };
-    return [];
-    // Write your code here.
-};
-
-function isPalindrome(string) {
-    let left = 0;
-    let right = string.length - 1;
-
-    while (left < right) {
-        if (string[left] !== string[right]) return false;
-        ++left;
-        --right;
-    };
-
-    return true;
-    // Write your code here.
-} 
